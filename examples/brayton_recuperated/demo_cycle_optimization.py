a343bc7a0b35c42a690882c5f52358fb8f21a818
roagr <roagr@dtu.dk>
2025-03-11 15:31:39 +0100

    update examples directory

--- examples/brayton_recuperated/demo_cycle_optimization.py
+++ examples/brayton_recuperated/demo_cycle_optimization.py
@@ -1,14 +1,15 @@
-import turboflow as tf
+import thermopt as th
 import matplotlib.pyplot as plt
 
 # Print package info
-tf.print_package_info()
+th.print_package_info()
 
 # Define configuration filename
-CONFIG_FILE = "case_sCO2_recompression.yaml"
+CONFIG_FILE = "case_sCO2_recuperated.yaml"
 
 # Initialize Brayton cycle problem
-cycle = tf.cycles.ThermodynamicCycleOptimization(CONFIG_FILE)
+cycle = th.ThermodynamicCycleOptimization(CONFIG_FILE)
+cycle.problem.plot_cycle_realtime(CONFIG_FILE)
 
 # Perform cycle opetimization
 cycle.run_optimization()
@@ -17,5 +18,3 @@ cycle.save_results()
 # Keep plots open
 plt.show()
 
-# optimizer.generate_output_files()
-# optimizer.create_animation()
