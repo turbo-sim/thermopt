eee4ace612e2cff604f3af485d3bc9acadb25cb8
Ivan Sidorov <s225683@dtu.dk>
2025-05-03 10:58:25 +0200

    Modelled the PTES system

--- examples/CO2_PTES/run_optimization.py
+++ examples/CO2_PTES/run_optimization.py
@@ -2,11 +2,17 @@ import thermopt as th
 import matplotlib.pyplot as plt
 
 # Print package info
-th.print_package_info()
+#th.print_package_info()
 
 # Define configuration filename
-# CONFIG_FILE = "./case_sCO2_heat_pump_valve.yaml"
-CONFIG_FILE = "./case_sCO2_heat_pump_turbine.yaml"
+CONFIG_FILE = "./case_sCO2_PTES.yaml"
+# config = th.read_configuration_file(CONFIG_FILE)
+# problem = th.ThermodynamicCycleProblem(config["problem_formulation"])
+# problem.fitness(problem.x0) #fitness: the same as calling evaluate_cycle function
+# #print(problem.cycle_data)
+# problem.plot_cycle()
+# #problem.plot_cycle_realtime(CONFIG_FILE, update_interval=0.1)
+# print(problem.cycle_data["energy_analysis"])
 
 # Initialize cycle problem
 cycle = th.ThermodynamicCycleOptimization(CONFIG_FILE)
@@ -17,9 +23,7 @@ cycle.run_optimization()
 cycle.save_results()
 
 # Create an animation of the optimization progress
-cycle.create_animation(format="mp4", fps=1)
+#cycle.create_animation(format="mp4", fps=1)
 
 # Keep plots open
 plt.show()
-
-
