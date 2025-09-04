32424c292fe05d473689552d1cd66f2c0fb1254d
roagr <roagr@dtu.dk>
2025-09-04 16:27:25 +0200

    Bump version: 0.2.1 → 0.2.2

--- thermopt/__init__.py
+++ thermopt/__init__.py
@@ -22,7 +22,7 @@ from .utilities import *
 set_plot_options()
 
 # Package info
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 PACKAGE_NAME = "thermopt"
 URL_GITHUB = "https://github.com/turbo-sim/thermopt"
 URL_DOCS = "https://turbo-sim.github.io/thermopt/"
